6ef5febdfe276b54668f00c3bcc56f33c9f019c1
Koji Matsuda <kjmatsuda1985@gmail.com>
2019-10-14 16:27:34 +0900

    10.6節。CppUMock を使用したテスト(`mock_c' に対する定義されていない参 照です)が出る状態

--- code-t0/tests/IO/FlashTest.cpp
+++ code-t0/tests/IO/FlashTest.cpp
@@ -1,10 +1,27 @@
+#include "CppUTest/TestHarness.h"
+#include "CppUTestExt/MockSupport.h"
+
 extern "C"
 {
+#include "CppUTestExt/MockSupport_c.h"
 #include "Flash.h"
-#include "MockIO.h"
+#include "m28w160ect.h"
 #include "FakeMicroTime.h"
+
+void IO_Write(ioAddress addr, ioData value)
+{
+	mock_c()->actualCall("IO_Write")
+		->withIntParameters("addr", addr)
+		->withIntParameters("value", value);
 }
-#include "CppUTest/TestHarness.h"
+
+ioData IO_Read(ioAddress addr)
+{
+	mock_c()->actualCall("IO_Read")
+		->withIntParameters("addr", addr);
+	return mock_c()->returnValue().value.intValue;
+}
+}
 
 TEST_GROUP(Flash)
 {
@@ -14,138 +31,141 @@ TEST_GROUP(Flash)
 
 	void setup()
 	{
-		address = 0x1000;
-		data = 0xBEEF;
+		address = 0xfeed;
+		data = 0x1dea;
 		result = -1;
-
-		MockIO_Create(20);
-		Flash_Create();
 	}
 
 	void teardown()
 	{
-		Flash_Destroy();
-		MockIO_Verify_Complete();
-		MockIO_Destroy();
+		mock().checkExpectations();
+		mock().clear();
 	}
 };
 
 TEST(Flash, WriteSucceeds_ReadyImmediately)
 {
-	int result = 0;
-	MockIO_Expect_Write(CommandRegister, ProgramCommand);
-	MockIO_Expect_Write(address, data);
-	MockIO_Expect_ReadThenReturn(StatusRegister, ReadyBit);
-	MockIO_Expect_ReadThenReturn(address, data);
+	mock().expectOneCall("IO_Write")
+			.withParameter("addr", CommandRegister)
+			.withParameter("value", ProgramCommand);
+	mock().expectOneCall("IO_Write")
+			.withParameter("addr", address)
+			.withParameter("value", data);
+	mock().expectOneCall("IO_Read")
+			.withParameter("addr", StatusRegister)
+			.andReturnValue(ReadyBit);
+	mock().expectOneCall("IO_Read")
+			.withParameter("addr", address)
+			.andReturnValue(data);
 
-	result = Flash_Write(address, data);
-
-	LONGS_EQUAL(FLASH_SUCCESS, result);
-}
-
-TEST(Flash, ProgramSucceedsNotImmediatelyReady)
-{
-	MockIO_Expect_Write(CommandRegister, ProgramCommand);
-	MockIO_Expect_Write(address, data);
-	MockIO_Expect_ReadThenReturn(StatusRegister, 0);
-	MockIO_Expect_ReadThenReturn(StatusRegister, 0);
-	MockIO_Expect_ReadThenReturn(StatusRegister, 0);
-	MockIO_Expect_ReadThenReturn(StatusRegister, ReadyBit);
-	MockIO_Expect_ReadThenReturn(address, data);
-
-	result = Flash_Write(address, data);
+	int result = Flash_Write(address, data);
 
 	LONGS_EQUAL(FLASH_SUCCESS, result);
 }
 
-TEST(Flash, WriteFails_VppError)
-{
-	MockIO_Expect_Write(CommandRegister, ProgramCommand);
-	MockIO_Expect_Write(address, data);
-	MockIO_Expect_ReadThenReturn(StatusRegister, ReadyBit | VppErrorBit);
-	MockIO_Expect_Write(CommandRegister, Reset);
-
-	result = Flash_Write(address, data);
-
-	LONGS_EQUAL(FLASH_VPP_ERROR, result);
-}
-
-TEST(Flash, WriteFails_ProgramError)
-{
-	MockIO_Expect_Write(CommandRegister, ProgramCommand);
-	MockIO_Expect_Write(address, data);
-	MockIO_Expect_ReadThenReturn(StatusRegister, ReadyBit | ProgramErrorBit);
-	MockIO_Expect_Write(CommandRegister, Reset);
-
-	result = Flash_Write(address, data);
-
-	LONGS_EQUAL(FLASH_PROGRAM_ERROR, result);
-}
-
-TEST(Flash, WriteFails_ProtectedBlockError)
-{
-	MockIO_Expect_Write(CommandRegister, ProgramCommand);
-	MockIO_Expect_Write(address, data);
-	MockIO_Expect_ReadThenReturn(StatusRegister, ReadyBit | ProtectedBlockErrorBit);
-	MockIO_Expect_Write(CommandRegister, Reset);
-
-	result = Flash_Write(address, data);
-
-	LONGS_EQUAL(FLASH_PROTECTED_BLOCK_ERROR, result);
-}
-
-TEST(Flash, WriteFails_FlashReadBackError)
-{
-	MockIO_Expect_Write(CommandRegister, ProgramCommand);
-	MockIO_Expect_Write(address, data);
-	MockIO_Expect_ReadThenReturn(StatusRegister, ReadyBit);
-	MockIO_Expect_ReadThenReturn(address, data - 1);
-
-	result = Flash_Write(address, data);
-
-	LONGS_EQUAL(FLASH_READ_BACK_ERROR, result);
-}
-
-TEST(Flash, WriteSucceeds_IgnoresOtherBitsUntilReady)
-{
-	MockIO_Expect_Write(CommandRegister, ProgramCommand);
-	MockIO_Expect_Write(address, data);
-	MockIO_Expect_ReadThenReturn(StatusRegister, ~ReadyBit);
-	MockIO_Expect_ReadThenReturn(StatusRegister, ReadyBit);
-	MockIO_Expect_ReadThenReturn(address, data);
-
-	result = Flash_Write(address, data);
-
-	LONGS_EQUAL(FLASH_SUCCESS, result);
-}
-
-TEST(Flash, WriteFails_Timeout)
-{
-	FakeMicroTime_Init(0, 500);
-	Flash_Create();
-
-	MockIO_Expect_Write(CommandRegister, ProgramCommand);
-	MockIO_Expect_Write(address, data);
-
-	for (int ii = 0; ii < 10; ii++)
-	{
-		MockIO_Expect_ReadThenReturn(StatusRegister, ~ReadyBit);
-	}
-
-	result = Flash_Write(address, data);
-	LONGS_EQUAL(FLASH_TIMEOUT_ERROR, result);
-}
-
-TEST(Flash, WriteFails_TimeoutAtEndOfTime)
-{
-	FakeMicroTime_Init(0xffffffff, 500);
-	Flash_Create();
-	MockIO_Expect_Write(CommandRegister, ProgramCommand);
-	MockIO_Expect_Write(address, data);
-	for (int ii = 0; ii < 10; ii++)
-	{
-		MockIO_Expect_ReadThenReturn(StatusRegister, ~ReadyBit);
-	}
-	result = Flash_Write(address, data);
-	LONGS_EQUAL(FLASH_TIMEOUT_ERROR, result);
-}
+//TEST(Flash, ProgramSucceedsNotImmediatelyReady)
+//{
+//	MockIO_Expect_Write(CommandRegister, ProgramCommand);
+//	MockIO_Expect_Write(address, data);
+//	MockIO_Expect_ReadThenReturn(StatusRegister, 0);
+//	MockIO_Expect_ReadThenReturn(StatusRegister, 0);
+//	MockIO_Expect_ReadThenReturn(StatusRegister, 0);
+//	MockIO_Expect_ReadThenReturn(StatusRegister, ReadyBit);
+//	MockIO_Expect_ReadThenReturn(address, data);
+//
+//	result = Flash_Write(address, data);
+//
+//	LONGS_EQUAL(FLASH_SUCCESS, result);
+//}
+//
+//TEST(Flash, WriteFails_VppError)
+//{
+//	MockIO_Expect_Write(CommandRegister, ProgramCommand);
+//	MockIO_Expect_Write(address, data);
+//	MockIO_Expect_ReadThenReturn(StatusRegister, ReadyBit | VppErrorBit);
+//	MockIO_Expect_Write(CommandRegister, Reset);
+//
+//	result = Flash_Write(address, data);
+//
+//	LONGS_EQUAL(FLASH_VPP_ERROR, result);
+//}
+//
+//TEST(Flash, WriteFails_ProgramError)
+//{
+//	MockIO_Expect_Write(CommandRegister, ProgramCommand);
+//	MockIO_Expect_Write(address, data);
+//	MockIO_Expect_ReadThenReturn(StatusRegister, ReadyBit | ProgramErrorBit);
+//	MockIO_Expect_Write(CommandRegister, Reset);
+//
+//	result = Flash_Write(address, data);
+//
+//	LONGS_EQUAL(FLASH_PROGRAM_ERROR, result);
+//}
+//
+//TEST(Flash, WriteFails_ProtectedBlockError)
+//{
+//	MockIO_Expect_Write(CommandRegister, ProgramCommand);
+//	MockIO_Expect_Write(address, data);
+//	MockIO_Expect_ReadThenReturn(StatusRegister, ReadyBit | ProtectedBlockErrorBit);
+//	MockIO_Expect_Write(CommandRegister, Reset);
+//
+//	result = Flash_Write(address, data);
+//
+//	LONGS_EQUAL(FLASH_PROTECTED_BLOCK_ERROR, result);
+//}
+//
+//TEST(Flash, WriteFails_FlashReadBackError)
+//{
+//	MockIO_Expect_Write(CommandRegister, ProgramCommand);
+//	MockIO_Expect_Write(address, data);
+//	MockIO_Expect_ReadThenReturn(StatusRegister, ReadyBit);
+//	MockIO_Expect_ReadThenReturn(address, data - 1);
+//
+//	result = Flash_Write(address, data);
+//
+//	LONGS_EQUAL(FLASH_READ_BACK_ERROR, result);
+//}
+//
+//TEST(Flash, WriteSucceeds_IgnoresOtherBitsUntilReady)
+//{
+//	MockIO_Expect_Write(CommandRegister, ProgramCommand);
+//	MockIO_Expect_Write(address, data);
+//	MockIO_Expect_ReadThenReturn(StatusRegister, ~ReadyBit);
+//	MockIO_Expect_ReadThenReturn(StatusRegister, ReadyBit);
+//	MockIO_Expect_ReadThenReturn(address, data);
+//
+//	result = Flash_Write(address, data);
+//
+//	LONGS_EQUAL(FLASH_SUCCESS, result);
+//}
+//
+//TEST(Flash, WriteFails_Timeout)
+//{
+//	FakeMicroTime_Init(0, 500);
+//	Flash_Create();
+//
+//	MockIO_Expect_Write(CommandRegister, ProgramCommand);
+//	MockIO_Expect_Write(address, data);
+//
+//	for (int ii = 0; ii < 10; ii++)
+//	{
+//		MockIO_Expect_ReadThenReturn(StatusRegister, ~ReadyBit);
+//	}
+//
+//	result = Flash_Write(address, data);
+//	LONGS_EQUAL(FLASH_TIMEOUT_ERROR, result);
+//}
+//
+//TEST(Flash, WriteFails_TimeoutAtEndOfTime)
+//{
+//	FakeMicroTime_Init(0xffffffff, 500);
+//	Flash_Create();
+//	MockIO_Expect_Write(CommandRegister, ProgramCommand);
+//	MockIO_Expect_Write(address, data);
+//	for (int ii = 0; ii < 10; ii++)
+//	{
+//		MockIO_Expect_ReadThenReturn(StatusRegister, ~ReadyBit);
+//	}
+//	result = Flash_Write(address, data);
+//	LONGS_EQUAL(FLASH_TIMEOUT_ERROR, result);
+//}
