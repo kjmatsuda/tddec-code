d8718b0fb1dca2a68b0be25a27322d44488c8a7c
Koji Matsuda <kjmatsuda1985@gmail.com>
2019-10-14 15:50:34 +0900

    10.4節。WriteFails_TimeoutAtEndOfTime

--- code-t0/tests/IO/FlashTest.cpp
+++ code-t0/tests/IO/FlashTest.cpp
@@ -136,3 +136,16 @@ TEST(Flash, WriteFails_Timeout)
 	LONGS_EQUAL(FLASH_TIMEOUT_ERROR, result);
 }
 
+TEST(Flash, WriteFails_TimeoutAtEndOfTime)
+{
+	FakeMicroTime_Init(0xffffffff, 500);
+	Flash_Create();
+	MockIO_Expect_Write(CommandRegister, ProgramCommand);
+	MockIO_Expect_Write(address, data);
+	for (int ii = 0; ii < 10; ii++)
+	{
+		MockIO_Expect_ReadThenReturn(StatusRegister, ~ReadyBit);
+	}
+	result = Flash_Write(address, data);
+	LONGS_EQUAL(FLASH_TIMEOUT_ERROR, result);
+}
